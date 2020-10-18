b076e77b1d9849c94a4b093fd9a4d04208e36059
Hiroyuki Morita <chiastolite.1980@gmail.com>
2020-10-19 05:48:03 +0900

    [from now] 2020/10/19 05:48:03
    diff --git a/installer/install-solargraph.sh b/installer/install-solargraph.sh
    index 6caaa70..0e6a9fe 100755
    --- a/installer/install-solargraph.sh
    +++ b/installer/install-solargraph.sh
    @@ -5,14 +5,14 @@ set -e
     # git clone --depth=1 https://github.com/castwide/solargraph .
     # bundle install --without development --path vendor/bundle
    
    -mkdir solargraph
    -gem install -i solargraph solargraph
    +mkdir -p vendor/bundle
    +gem install -i vendor/bundle solargraph
    
     cat <<EOF >solargraph
     #!/usr/bin/env bash
    
    -DIR=\$(cd \$(dirname \$0); pwd)/solargraph
    -GEM_PATH=\$DIR/solargraph/ \$DIR/bin/solargraph \$*
    +DIR=\$(cd \$(dirname \$0); pwd)/vendor/bundle
    +GEM_PATH=\$DIR/ \$DIR/bin/solargraph \$*
     EOF
    
     chmod +x solargraph

--- installer/install-solargraph.sh
+++ installer/install-solargraph.sh
@@ -5,14 +5,14 @@ set -e
 # git clone --depth=1 https://github.com/castwide/solargraph .
 # bundle install --without development --path vendor/bundle
 
-mkdir solargraph
-gem install -i solargraph solargraph
+mkdir -p vendor/bundle
+gem install -i vendor/bundle solargraph
 
 cat <<EOF >solargraph
 #!/usr/bin/env bash
 
-DIR=\$(cd \$(dirname \$0); pwd)/solargraph
-GEM_PATH=\$DIR/solargraph/ \$DIR/bin/solargraph \$*
+DIR=\$(cd \$(dirname \$0); pwd)/vendor/bundle
+GEM_PATH=\$DIR/ \$DIR/bin/solargraph \$*
 EOF
 
 chmod +x solargraph
